92449d5f40cc639964ff14cfdc36b1b53c622b2c
qwaszx000 <kalita1303@gmail.com>
2025-03-03 10:45:37 -0500

    New data collection

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -4,54 +4,73 @@ import time
 import subprocess
 
 #rust_faf is not working, so we exclude it for now
-exclude_dirs = ["utils", "results", "venv", ".vscode", ".git", "rust_faf"]
+exclude_dirs = ["utils", "results", "venv", ".vscode", ".git", "rust_faf", "docker_stats", "imgs"]
 
 local_port = 8081
 target_url = "http://127.0.0.1:%d/test_plain" % local_port
+
 results_dir = "results"
+docker_stats_dir = "docker_stats"
+
 benchmarking_cmds = {
     "wrk": ["utils/wrk", "-c", "50", "-t", "2", "-d", "10s", "--latency", target_url],
     "go-wrk": ["utils/go-wrk", "-c", "50", "-d", "10", "-cpus", "2", target_url],
     "my_util": ["utils/load_simulator/load_simulator", "-rps", "0", "-workers", "50", "-target", target_url]
 }
 
-docker_build_env = {
-    "DOCKER_BUILDKIT": "1"
-}
-docker_build_cmd = ["docker", "build", "-q"]
 memory_limit = 256*1024*1024 #256MiB
-docker_run_cmd = [
-    "docker", "run",
-    "--memory", str(memory_limit), "--memory-swap", str(memory_limit*2),
-    "--publish", "%d:8080" % local_port,
-    "--label", "benchmark_run=1"
-]
 
-docker_ps_cmd = ["docker", "ps", "-q", "--filter", "label=benchmark_run=1"]
+def build_image(dir_name):
+    docker_build_env = {
+        "DOCKER_BUILDKIT": "1"
+    }
+    docker_build_cmd = ["docker", "build", "-q"]
 
-docker_stop_cmd = ["docker", "stop"]
-
-def docker_run(dir_name):
-    #Build docker container and get it's id
     _cmd = docker_build_cmd + [dir_name] #functional append -> return new list instead of changing initial one
     docker_build_instance = subprocess.Popen(_cmd, bufsize=1, text=True, stdout=subprocess.PIPE, env=docker_build_env)
     image_id_str, _ = docker_build_instance.communicate()
     image_id_str = image_id_str.strip()
+
+    return image_id_str
+
+def get_container_id():
+    docker_ps_cmd = ["docker", "ps", "-q", "--filter", "label=benchmark_run=1"]
+    proc = subprocess.Popen(docker_ps_cmd, bufsize=1, text=True, stdout=subprocess.PIPE)
+    container_id_str, _ = proc.communicate()
+    container_id_str = container_id_str.strip()
+
+    return container_id_str
+
+def log_docker_stats_to(fd):
+    docker_stats_cmd = ["docker", "stats"]
+    proc = subprocess.Popen(docker_stats_cmd, bufsize=1, text=True, stdout=fd, stderr=subprocess.STDOUT)
+
+    return proc
+
+def docker_run(dir_name):
+    docker_run_cmd = [
+        "docker", "run",
+        "--memory", str(memory_limit), "--memory-swap", str(memory_limit*2),
+        "--publish", "%d:8080" % local_port,
+        "--label", "benchmark_run=1"
+    ]
+
+    #Build docker container and get it's id
+    image_id_str = build_image(dir_name)
     print(image_id_str)
 
-    #Run docker container, sleep 5s to let it start
+    #Run docker container, sleep to let it start
     _cmd = docker_run_cmd + [image_id_str]
     print(_cmd)
     docker_run_instance = subprocess.Popen(_cmd)
     time.sleep(10)
 
     #Get container id to stop
-    proc = subprocess.Popen(docker_ps_cmd, bufsize=1, text=True, stdout=subprocess.PIPE)
-    container_id_str, _ = proc.communicate()
-    container_id_str = container_id_str.strip()
+    container_id_str = get_container_id()
     print(container_id_str)
 
     def stop_function():
+        docker_stop_cmd = ["docker", "stop"]
         subprocess.run(docker_stop_cmd + [container_id_str])
 
     return stop_function
@@ -75,11 +94,18 @@ def docker_compose_run(dir_name):
 docker_compose_apps = ["php_symfony"]
 def run_benchmark(dir_name):
     print(dir_name)
-    
+
     if dir_name in docker_compose_apps:
         stop_docker_fn = docker_compose_run(dir_name)
     else:
         stop_docker_fn = docker_run(dir_name)
+
+    docker_stats_file_path = os.path.join(docker_stats_dir, dir_name)
+    docker_stats_fd = open(docker_stats_file_path, "w")
+    stats_proc = log_docker_stats_to(docker_stats_fd)
+
+    #Quiet time to log normal memory and cpu usage
+    time.sleep(5)
 
     #Run stress testing tools with redirecting output to txt files
     for tool_name, _tool_cmd in benchmarking_cmds.items():
@@ -90,6 +116,14 @@ def run_benchmark(dir_name):
             subprocess.run(_tool_cmd, bufsize=1, text=True, stdout=f, stderr=subprocess.STDOUT)
 
     stop_docker_fn()
+
+    time.sleep(5)
+    stats_proc.terminate()
+    try:
+        stats_proc.wait(10)
+    except:
+        stats_proc.kill()
+    docker_stats_fd.close()
 
 def main():
     for entity in os.scandir("./"):
--- utils/top_statistics.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-top -b -d 1 -U $(id -u) 2>/dev/null >> /home/server/top_output.txt;
-
